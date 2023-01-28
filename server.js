fbc529122a7b23a6fa5d76df1ea9100cce2384a7
Damian Goodenough <dgoodenough@proton.me>
2023-01-28 13:12:28 -0800

    connect to mongoDB in the cloud

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const queryType = require('query-types');
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false); 
+
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.kph3znu.mongodb.net/?retryWrites=true&w=majority`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Connected to MongoDB Atlas cluster!');
+});
 
 const app = express();
 
