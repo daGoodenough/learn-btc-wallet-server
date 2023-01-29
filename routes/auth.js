c3c487f8073453da9d4b444e9cfa6ffdcb9a8d77
Damian Goodenough <dgoodenough@proton.me>
2023-01-29 10:31:30 -0800

    sign in auth route

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,11 @@
 const router = require('express').Router();
+const passport = require('passport');
+const passportServices = require('../services/passport');
 const Authentication = require('../controllers/authentication');
 
+const requireSignin = passport.authenticate('local', {session: false});
+
+router.post('/signin', requireSignin, Authentication.signin);
 router.post('/signup', Authentication.signup)
 
 module.exports = router;
