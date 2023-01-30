03b100f2df69456dba0d8f84263058e393e35646
Damian Goodenough <dgoodenough@proton.me>
2023-01-30 10:41:33 -0800

    create key Model

---
+++ models/key.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
+const { Schema } = mongoose;
+
+const KeySchema = new Schema({
+  privateKey: String,
+  publicKey: String,
+  isCompressed: Boolean,
+  network: String,
+  keyName: String,
+});
+
+const KeyModel = mongoose.model('Key', KeySchema);
+
+module.exports = KeyModel;
