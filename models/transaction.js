09bca6dfcf19b92a8280c6c158d77c8b72efde21
Damian Goodenough <dgoodenough@proton.me>
2023-01-30 10:41:45 -0800

    create transaction model

---
+++ models/transaction.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+mongoose.set('stringQuery', false);
+const { Schema, model } = mongoose;
+const UserModel = require('./user');
+
+const TransactionSchema = new Schema({
+  sender: {type: Schema.Types.ObjectId, ref: 'User'},
+  recipient: {type: Schema.Types.ObjectId, ref: 'User'},
+  amountSent: Number,
+  fee: Number,
+})
+
+const TransactionModel = model('Transaction', TransactionSchema);
+
+module.exports = TransactionModel;
