f991bbc9654275eda2fd3f72a43b380d81cfe776
Damian Goodenough <dgoodenough@proton.me>
2023-01-30 10:41:54 -0800

    create wallet model

---
+++ models/wallet.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+mongoose.set('stringQuery', false);
+const { Schema, model } = mongoose;
+// const TransactionSchema = requrie('./transaction');
+const KeySchema = require('./key');
+
+const WalletSchema = new Schema({
+  address: String,
+  keys: [KeySchema],
+  type: String,
+  balance: Number,
+  transactions: [{type: Schema.Types.ObjectId, ref: 'Transaction'}]
+})
+
+const WalletModel = model('Wallet', WalletSchema);
+
+module.exports = WalletModel;
