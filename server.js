6b449c1bb11178d5d74bdf77cfefeca3be6bfeb3
Damian Goodenough <dgoodenough@proton.me>
2023-02-02 11:09:19 -0800

    set up cors

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const queryType = require('query-types');
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', false); 
+const cors = require('cors');
 
 mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.kph3znu.mongodb.net/?retryWrites=true&w=majority`, {
   useNewUrlParser: true,
@@ -19,6 +20,7 @@ db.once('open', function() {
 
 const app = express();
 
+app.use(cors({credentials: true, origin: true}))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(queryType.middleware());
@@ -26,7 +28,7 @@ app.use(queryType.middleware());
 const router = require('./routes')
 app.use(router);
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server listening on port ${5000}`)
 })
