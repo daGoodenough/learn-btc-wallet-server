bce0d79cd3b21130b82bc72595822667278be53f
Damian Goodenough <dgoodenough@proton.me>
2023-02-09 21:22:56 -0800

    create faucet and coinbase functions

--- bitcoin/rpcUtils.js
+++ bitcoin/rpcUtils.js
@@ -12,3 +12,33 @@ module.exports.getAddrBalance = async (address) => {
     );
   });
 }
+
+module.exports.faucet = async (address, amount = 1, fee = 0.00001) => {
+  return new Promise((resolve, reject) => {
+    rpc('settxfee', [fee], (err, response) => {
+      if (err) { reject(err); };
+      resolve(response);
+    });
+  })
+    .then(response => new Promise((resolve, reject) => {
+      if (!response) { reject() };
+
+      rpc('sendtoaddress', [address, amount], (err, txid) => {
+        if (err) { reject(err); };
+        resolve(txid);
+      });
+    }))
+    .catch(error => {
+      throw new Error(error);
+    });
+};
+
+module.exports.coinbase = async (address, amount = 1) => {
+  return new Promise((resolve, reject) => {
+    rpc('generatetoaddress', [amount, address], (err, blocks) => {
+      if (err) { reject(err); };
+
+      resolve(blocks);
+    });
+  });
+}
