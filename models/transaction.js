8704666f1f935569ea80908978233ea0f6c4505e
Damian Goodenough <dgoodenough@proton.me>
2023-02-10 19:57:18 -0800

    change tx model to indlude scriptPubKey and address

--- models/transaction.js
+++ models/transaction.js
@@ -6,10 +6,11 @@ const UserModel = require('./user');
 const TransactionSchema = new Schema({
   sender: {type: Schema.Types.ObjectId, ref: 'User'},
   recipient: {type: Schema.Types.ObjectId, ref: 'User'},
-  amountSent: Number,
-  fee: Number,
+  amount: Number,
   txid: String,
   coinbase: Boolean,
+  scriptPubKey: String,
+  address: String,
 })
 
 const TransactionModel = model('Transaction', TransactionSchema);
