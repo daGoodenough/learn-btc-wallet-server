56d67defa7cd213589d6e961c4b42f1c59dd76cd
Damian Goodenough <dgoodenough@proton.me>
2023-02-12 13:47:43 -0800

    GET decoded transactions from id

--- routes/transactions.js
+++ routes/transactions.js
@@ -1,10 +1,28 @@
 const router = require('express').Router();
 const { requireAuth } = require('../services/passport');
 const { fundWallet, createRawTx, broadcastRaw } = require('../bitcoin/transactions');
+const { getTx } = require('../bitcoin/rpcUtils');
 
 router.post('/fund-wallet', requireAuth, fundWallet);
+
 router.post('/create-raw/p2pkh', requireAuth, createRawTx);
+
 router.post('/broadcast', requireAuth, broadcastRaw);
+
+router.get('/raw', async (req, res) => {
+  try {
+    const { txid } = req.query;
+    if (!txid) {
+      return res.end();
+    }
+
+    const decoded = await getTx(txid);
+
+    res.json(decoded);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+})
 
 
 module.exports = router;
